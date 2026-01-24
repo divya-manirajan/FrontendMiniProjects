c9e59474f1707ae5a63bf98db53f624dc182422d
divya-manirajan <divya.manirajan@gmail.com>
2026-01-24 17:19:50 -0600

    fleshed out conditional redering and main page

--- src/app/ConditionalRendering/page.js
+++ src/app/ConditionalRendering/page.js
@@ -1,16 +1,26 @@
 'use client'
+
 import { useState } from "react"
-export default function Test(){
 
-    const [editInfo, setEditInfo] = useState({
-        isEdit: false,
-        editIndex: -1
-    })
+export default function page(){
+    
+    const [condition, setCondition] = useState()
 
-    if (editInfo.isEdit == true){
-        return <p>TRUE</p>
-    }
-    if (editInfo.isEdit == false){
-        return <p>FALSE</p>
-    }
+    return(
+        <div className="h-screen w-screen flex flex-col items-center bg-violet-300">
+            {condition ? (
+                <div className="h-24 w-48 bg-green-900 my-20 flex justify-center items-center text-white rounded-2xl">TRUE</div>
+            ) : (
+                <div className="h-24 w-48 bg-red-900 my-20 flex justify-center items-center text-white rounded-2xl">FALSE</div>
+            )}
+           
+            <div className="bg-gray-400 h-48 w-56 rounded-3xl flex justify-center items-center">
+                <button 
+                    onClick={() => {setCondition(!condition)}}
+                    className="bg-blue-400 p-3 rounded-xl hover:shadow-xl hover:cursor-pointer">Click Me</button>
+            </div>
+        </div>
+    )
+
+    
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,20 @@
 import Link from "next/link";
 
 export default function Home(){
-  return(<>
-    <h1>List of projects:</h1>
-    <h1>Imitate UI design of youtube or balatro with Tailwind</h1>
-    <h1>Set up some local storage with JSON and create a "full stack" project</h1>
-    <h1>Connect to a database - SQLLite</h1>
-
-    <div className="flex flex-col items-center">
-      <Link className="bg-blue-200 w-fit p-2 m-2" href={'/ToDo'}>ToDo</Link>
-      <Link className="bg-blue-200 w-fit p-2 m-2" href={'/MultiVariableForm'}>MultiVariableForm</ Link>
-      <Link className="bg-blue-200 w-fit p-2 m-2" href={'/ConditionalRendering'}>ConditionalRendering</ Link>
-      <Link className="bg-blue-200 w-fit p-2 m-2" href={'/BalatroUI'}>BalatroUI</Link>
+  return(
+  <div className="h-screen w-screen bg-green-100 p-10 flex flex-col items-center">
+    <div className="grid grid-cols-2 place-items-center w-120">
+      <Link className="bg-blue-200 w-48 h-14 p-2 m-2 flex justify-center items-center hover:cursor-pointer hover:shadow-xl" href={'/ToDo'}>ToDo</Link>
+      <Link className="bg-blue-200 w-48 h-14 p-2 m-2 flex justify-center items-center hover:cursor-pointer hover:shadow-xl" href={'/MultiVariableForm'}>MultiVariableForm</ Link>
+      <Link className="bg-blue-200 w-48 h-14 p-2 m-2 flex justify-center items-center hover:cursor-pointer hover:shadow-xl" href={'/ConditionalRendering'}>ConditionalRendering</ Link>
+      <Link className="bg-blue-200 w-48 h-14 p-2 m-2 flex justify-center items-center hover:cursor-pointer hover:shadow-xl" href={'/BalatroUI'}>BalatroUI</Link>
     </div>
-  </>)
+
+    <div className="text-center mt-10">
+      <h1>List of upcoming projects:</h1>
+      <h1>1. Set up some local storage with JSON and create a "full stack" project</h1>
+      <h1>2. Connect to a database - SQLLite</h1>
+    </div>
+    
+  </div>)
 }
